370471bb91d74d499981d624d785f4582085ea0b
4cidru <59209802+4cidru@users.noreply.github.com>
2025-02-10 23:10:53 -0500

    Update backend.js

--- Matchroom System/backend/backend.js
+++ Matchroom System/backend/backend.js
@@ -1,4 +1,7 @@
 require('dotenv').config(); // Load environment variables
+const PORT = process.env.PORT || 5000;
+
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
 const email = process.env.EMAIL;
 const emailPassword = process.env.EMAIL_PASSWORD;
