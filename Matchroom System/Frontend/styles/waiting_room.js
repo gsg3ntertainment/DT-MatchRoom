c393ec33e085aaf0a7fc1434c4a35b7174e882af
4cidru <59209802+4cidru@users.noreply.github.com>
2025-02-15 23:57:26 -0500

    Update waiting_room.js

--- Matchroom System/Frontend/styles/waiting_room.js
+++ Matchroom System/Frontend/styles/waiting_room.js
@@ -1,7 +1,7 @@
 import { useEffect, useState } from 'react';
 import io from 'socket.io-client';
 
-const socket = io('https://your-deployed-backend.on.render.com'); // Replace with your backend URL
+const socket = io('https://dt-matchroom.onrender.com'); // Replace with your backend URL
 
 export default function WaitingRoom() {
     const [waitingUsers, setWaitingUsers] = useState([]);
